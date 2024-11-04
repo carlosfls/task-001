64a8fbd3852f4ffb51770ae2266fa7e2fb800a19
carlos <carlosfls54@gmail.com>
2024-11-03 22:07:01 -0400

    refactor added missing getters and setters

--- cuenta_movimientos_api/src/main/java/com/test/cuenta_movimientos_api/services/cuentas/dto/UpdateCuentaDTO.java
+++ cuenta_movimientos_api/src/main/java/com/test/cuenta_movimientos_api/services/cuentas/dto/UpdateCuentaDTO.java
@@ -1,5 +1,10 @@
 package com.test.cuenta_movimientos_api.services.cuentas.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UpdateCuentaDTO {
 
     private Boolean estado;
